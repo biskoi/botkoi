4b6935bcbf0fc0f012dbbb04566405855d09055b
biskoi <biskoi@daydream.cafe>
2021-02-21 16:35:39 -0700

    up to current

--- index.js
+++ index.js
@@ -9,10 +9,32 @@ server.once('ready', () => {
 server.login(process.env.TOKEN)
 
 server.on('message', (message) => {
-   let split = message.split(' ')
-   console.log(split)
-   if (message.content === 'goo goo') {
-      message.channel.send('gaa gaa')
+   // let split = message.split(' ')
+   console.log('STARTSTARTSTARTSTARTSTARTSTARTSTARTSTARTSTARTSTARTSTART', message)
+   
+   switch (message.content) {
+      case '!bonk':
+         message.channel.send('https://media1.tenor.com/images/6493bee2be7ae168a5ef7a68cf751868/tenor.gif?itemid=17298755')
+         break
+      
+      case '!jail':
+         message.channel.send('https://i.imgur.com/D33g4eu.png')
+         break
+
+      case '!jailbreak':
+         message.channel.send('https://i.imgur.com/KTO0vh4.png')
+         break
+   
+      case 'goo goo':
+         message.channel.send('gaa gaa')
+         break
+
+      case 'pee pee':
+         message.channel.send('poo poo')
+         break
+
+      default:
+         break;
    }
 });
 
