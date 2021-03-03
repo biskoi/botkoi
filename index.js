06d637595794c412cdb560ddc7edb67fd317c0b4
biskoi <biskoi@daydream.cafe>
2021-03-02 20:22:05 -0700

    add playback functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const discord = require('discord.js')
 require('dotenv').config()
+const ytdl = require("ytdl-core");
 const server = new discord.Client();
 
 server.once('ready', () => {
@@ -8,11 +9,31 @@ server.once('ready', () => {
 
 server.login(process.env.TOKEN)
 
-server.on('message', (message) => {
+let queue = new Map()
+
+server.on('message', async (message) => {
    // let split = message.split(' ')
-   console.log('STARTSTARTSTARTSTARTSTARTSTARTSTARTSTARTSTARTSTARTSTART', message)
+   if (message.author.bot || !message.content.startsWith('!')) {
+      console.log('not for bot')
+      return
+   }
+
+   // console.log('STARTSTARTSTARTSTARTSTARTSTARTSTARTSTARTSTARTSTARTSTART', message)
+   console.log
    
-   switch (message.content) {
+   switch (message.content.split(' ')[0]) {
+      case '!skip':
+         skip(message, queue.get(message.guild.id))
+         break;
+
+      case '!pause':
+         stop(message, queue.get(message.guild.id))
+         break;
+
+      case '!play':
+         joinVoip(message, queue.get(message.guild.id))
+         break;
+
       case '!bonk':
          message.channel.send('https://media1.tenor.com/images/6493bee2be7ae168a5ef7a68cf751868/tenor.gif?itemid=17298755')
          break
@@ -42,13 +63,86 @@ server.on('message', (message) => {
    }
 });
 
+async function joinVoip(message, serverQueue) {
+   const args = message.content.split(" ");
+   if (message.member.voice.channel) {
+      const songInfo = await ytdl.getInfo(args[1]);
+      const song = {
+         title: songInfo.videoDetails.title,
+         url: songInfo.videoDetails.video_url,
+      };
+      const voiceChannel = message.member.voice.channel;
 
-const data = [1, 2, 3]
+      if (!serverQueue) {
+         const queueContruct = {
+            textChannel: message.channel,
+            voiceChannel: voiceChannel,
+            connection: null,
+            songs: [],
+            volume: 4,
+            playing: true
+         };
+      
+         queue.set(message.guild.id, queueContruct);
+         queueContruct.songs.push(song);
 
-const updatedData = data.filter((item) => {
- return item === 3
-})
+         
+         try {
+            var connection = await voiceChannel.join();
+            queueContruct.connection = connection;
+            play(message.guild, queueContruct.songs[0]);
+         } 
+         catch (err) {
+            console.log(err);
+            queue.delete(message.guild.id);
+            return message.channel.send(err);
+         }
+      } else {
+      serverQueue.songs.push(song);
+      return message.channel.send(`${song.title} added`);
+      };
 
-const newData = [2]
+   };
+};
 
-console.log(updatedData)
+function skip(message, serverQueue) {
+   if (!message.member.voice.channel)
+     return message.channel.send(
+       "NO"
+     );
+   if (!serverQueue)
+     return message.channel.send("no");
+   serverQueue.connection.dispatcher.end();
+ }
+ 
+ function stop(message, serverQueue) {
+   if (!message.member.voice.channel)
+     return message.channel.send(
+       "NO"
+     );
+     
+   if (!serverQueue)
+     return message.channel.send("NOOO");
+     
+   serverQueue.songs = [];
+   serverQueue.connection.dispatcher.end();
+ }
+ 
+ function play(guild, song) {
+   const serverQueue = queue.get(guild.id);
+   if (!song) {
+     serverQueue.voiceChannel.leave();
+     queue.delete(guild.id);
+     return;
+   }
+ 
+   const dispatcher = serverQueue.connection
+     .play(ytdl(song.url))
+     .on("finish", () => {
+       serverQueue.songs.shift();
+       play(guild, serverQueue.songs[0]);
+     })
+     .on("error", error => console.error(error));
+   dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+   // serverQueue.textChannel.send(`Start playing: **${song.title}**`);
+ }
